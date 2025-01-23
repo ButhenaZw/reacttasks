10e5d0faaae8bc0449cbc4ffe1ca9ac52b244bee
ButhenaZw <bouthena762002@gmail.com>
2025-01-23 20:40:26 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,8 @@
 
-import Task1 from './Task/day2/task1';
-import Task2 from './Task/day2/task2';
-import Task3 from './Task/day2/task3';
-import Task4 from './Task/day2/task4';
-import Task5 from './Task/day2/task5';
-import img from './Task/day2/1708250582111-ezgif.com-webp-to-jpg-converter.jpg'
-import Task6 from './Task/day2/task6';
-import Task7 from './Task/day2/task7';
-import Task8 from './Task/day2/task8';
-import Task9 from './Task/day2/task9';
-import Task10 from './Task/day2/task10';
-import Task11 from './Task/day2/task11';
-
-
-
-
 function App() {
   return (
     <div>
-       
-      <Task1 name="Amro"/><hr/>
-      <Task2 label="Click me!"/><hr/>
-      <Task3 productName="Laptop" price="$999" description="A powerful laptop for work and play."/><hr/>
-      <Task4 name="Amro" email="amro@orange.com" age="27"/><hr/>
-      <Task5 avatar="https://media.discordapp.net/attachments/1314516379524923446/1314516749189775470/1708250582111.jpg?ex=6792051f&is=6790b39f&hm=c69298d80c095e0f65ce311f1232c431ae71849e57a1378d01a4cc72945ba539&=&format=webp&width=582&height=582" name="Amro" email="amro@orange.com"/><hr/>
-      <Task6 title="My Website" hLink="" aLink="" ctLink="" /><hr/>
-      <Task7 label="click me" /><hr/>
-      <Task8/><hr/>
-      <Task9/><hr/>
-      <Task10/><hr/>
-      <Task11/><hr/>
-
-
-
-
+      <h1>His</h1>
     </div>
   );
 }
---
+++ src/Task/day3/about.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+
+function About(){
+    return(
+        <div>
+            <h1>About Page</h1>
+        </div>
+    )
+}
+export default About;
